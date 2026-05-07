c3fe061982c771f869d04b5a5e513a26661a20cc
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2026-05-06 23:28:16 -0300

    Adição de rota inicial e refatoração no schema

--- src/modules/auth/schemas/index.ts
+++ src/modules/auth/schemas/index.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
 export const registerSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(6)
+  name: z.string().min(2).nonempty(),
+  email: z.string().email().nonempty(),
+  password: z.string().min(6).nonempty()
 })
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6)
+  email: z.string().email().nonempty(),
+  password: z.string().min(6).nonempty()
 })
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import { financeRoutes } from '../modules/finance/finance.routes'
 
 const router = Router()
 
+router.get('/', (req, res) => {
+  res.send('API Quintela Cred ON')
+})
 router.use('/auth', authRoutes)
 router.use('/finance', financeRoutes)
 
