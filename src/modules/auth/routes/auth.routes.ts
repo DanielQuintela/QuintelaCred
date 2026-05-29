4b682277de948aaeb144bff7161020b686ec74d8
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2026-05-29 00:51:36 -0300

    Adição de busca automatizada de usuário logado

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { AuthController } from '../auth.controller'
 import { validateDto } from '../../../middlewares'
 import { registerSchema, loginSchema } from '../schemas'
+import { authMiddleware } from '../../../middlewares/auth'
 
 const router = Router()
 const controller = new AuthController()
 
 router.post('/register', validateDto(registerSchema), controller.register)
 router.post('/login', validateDto(loginSchema), controller.login)
+router.get('/me', authMiddleware, controller.findMe)
 
 export { router as authRoutes }
