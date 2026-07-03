f59e075d2857f2addb434d0931e3cf24c1465ea1
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2026-07-03 18:50:47 -0300

    Refatoração da rota e schemas

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import { AuthController } from '../auth.controller'
 import { validateDto } from '../../../middlewares'
-import { registerSchema, loginSchema } from '../schemas'
-import { authMiddleware } from '../../../middlewares/auth'
+import { registerSchema, loginSchema, registerAdminSchema } from '../schemas'
+import { adminMiddleware, authMiddleware } from '../../../middlewares/auth'
 
 const router = Router()
 const controller = new AuthController()
 
-router.post('/register', validateDto(registerSchema), controller.register)
+router.post('/register', authMiddleware, adminMiddleware, validateDto(registerSchema), controller.register)
 router.post('/login', validateDto(loginSchema), controller.login)
 router.get('/user', authMiddleware, controller.findMe)
+router.post('/admin', validateDto(registerAdminSchema), controller.registerAdmin)
 
 export { router as authRoutes }
