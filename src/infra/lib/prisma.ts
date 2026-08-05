824204779f9f1305f230cc14e656b504d15475e2
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2026-08-04 21:55:31 -0300

    Correção da infra conexão com o banco

--- src/infra/lib/prisma.ts
+++ src/infra/lib/prisma.ts
@@ -1,11 +1,22 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
+// Cria o pool de conexões usando o pacote 'pg'
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// Passa a instância do Pool para o adaptador do Prisma
+const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+// Reaproveita a mesma instância em recargas do Serverless
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
